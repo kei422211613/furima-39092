d04b7834fa105b0552b969bc7910369ab1df19b5
kei422211613 <126162376+kei422211613@users.noreply.github.com>
2023-04-03 16:59:35 +0900

    新規登録画面の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
- 
+
   private
 
   def basic_auth
@@ -11,6 +11,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth])
   end
 end
