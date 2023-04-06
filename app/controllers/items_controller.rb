69b3bdb80f52eb43e0456fc6cfc367d696d5ff73
kei422211613 <126162376+kei422211613@users.noreply.github.com>
2023-04-06 17:07:25 +0900

    itemモデルの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,21 @@
 class ItemsController < ApplicationController
 
   def index
+    @items = Item.all
   end
   
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Iten.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :image, :text)
+  end
+
 end
  
