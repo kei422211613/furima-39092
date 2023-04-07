4f493d511ae4ce5459e1e9aecdbd903e9f79a605
kei422211613 <126162376+kei422211613@users.noreply.github.com>
2023-04-07 17:30:54 +0900

    画像表示の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,13 +4,14 @@ class Item < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  belongs_to :user
+  has_one_attached :image
+  
   validates :name, presence: true
   validates :description, presence: true
   validates :category, presence: true, inclusion: { in: %w(--- メンズ レディース ベビー・キッズ インテリア・住まい・小物 本・音楽・ゲーム おもちゃ・ホビー・グッズ 家電・スマホ・カメラ スポーツ・レジャー ハンドメイド その他) }
   validates :condition, presence: true, inclusion: { in: %w(--- 新品・未使用 未使用に近い 目立った傷や汚れなし やや傷や汚れあり 傷や汚れあり 全体的に状態が悪い) }
   validates :shipping_fee_payer, presence: true, inclusion: { in: %w(--- 着払い(購入者負担) 送料込み(出品者負担) ) }
-  validates :prefecture_code, presence: true, inclusion: { in: JpPrefecture::Prefecture.all.map(&:code).unshift('---') }
   validates :shipping_duration, presence: true, inclusion: { in: %w(--- 1~2日で発送 2~3日で発送 4~7日で発送) }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :items
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:index, :new, :create]
+  resources :users, only: [:index, :new, :create] do
+    resources :items, only: [:new, :create]
+  end
 end
