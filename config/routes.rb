14397329c31bb3dafe4bd2cf9f8a5186bd0548e8
kei422211613 <126162376+kei422211613@users.noreply.github.com>
2023-04-11 20:58:01 +0900

    商品詳細表示機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:new, :create] do
-    #resources :orders, only: [:index]
+  resources :items, only: [:new, :create, :show] do
+    resources :orders, only: [:index]
   end
 end
